776ef85084af07fd559364a2f0c59d714cdbf84f
viswath12 <E0167289@u.nus.edu>
2017-08-10 18:03:54 +0800

    Added cards

--- src/app/model/uno.ts
+++ src/app/model/uno.ts
@@ -7,11 +7,13 @@ export interface Card {
 
 export class UnoDeck {
 
+    private static colours = ["Red", "Yellow", "Green", "Blue"];
     private cards:Card[] = [];
 
     constructor(){
 
         for(let i = 0; i<2; i++) {
+            for(let c = 0; c<UnoDeck.colours.length; c++) {
                 for(let j=0;j<12; j++) {
                     let prefix = "0";
                     if(j<10) {
@@ -20,8 +22,30 @@ export class UnoDeck {
                     else {
                         prefix = j + "";
                     }
+                    this.cards.push({value: j+1, 
+                        colour: UnoDeck.colours[c],
+                    image: "/assets/uno_deck/c0" +c+"_" +prefix+".png"});
+                    }        
                 }
             }
+        for(let i = 0; i<4; i++) {
+        this.cards.push({value: 0, 
+                        colour: UnoDeck.colours[i],
+                    image: "/assets/uno_deck/c" + i + "_00.png"});
+                    } 
+        for(let i = 0; i<4; i++) {
+        this.cards.push({value: 13, 
+                        colour: "All 4 Colours",
+                    image: "/assets/uno_deck/c4_00.png"});
+                    }
+
+        for(let i = 0; i<4; i++) {
+        this.cards.push({value: 14, 
+                        colour: "Plus 4 Cards",
+                    image: "/assets/uno_deck/c4_01.png"});
+                    }
+
+        }
+        
 
     }
-}
